cb59c7ab57cfb275cd07f0aa3a3b2030432226e0
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-03-26 00:53:39 -0400

    Create q169.py

---
+++ array/easy/q169.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 02:24:53 2019
+
+@author: pengz
+"""
+
+'''
+Given an array of size n, find the majority element. The majority element is the element that 
+appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+'''
+
+def majorityElement(nums):## Written by my own, using dict, time complexity is O(N), since find in dict is O(1) 
+    times = int(len(nums)/2)+1
+    ret = {}
+    for num in nums:
+        if num not in ret.keys():
+            ret[num] = 1
+        else:
+            ret[num] += 1
+        if ret[num] == times:
+            return num
+
+a = majorityElement([2,2,1,1,1,2,2,3])
