661c0dca3870adfa9640784ffb6ebe98d6ae63d1
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-04-26 02:16:01 -0400

    Update q350.py

--- hashtable/easy/q350.py
+++ hashtable/easy/q350.py
@@ -30,7 +30,7 @@ Follow up:
     What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
 '''
-def intersect(nums1, nums2):   ## Still using two dicts, written by my own
+def intersect(nums1, nums2):   ## Still using two dicts, written by my own, but this one is limited by memory
     ret = []
     ans1 = {}
     ans2 = {}
@@ -51,6 +51,21 @@ def intersect(nums1, nums2):   ## Still using two dicts, written by my own
             else:
                 ans2[item] = 1
                 ret.append(item)
+    return ret
+
+def intersect2(nums1, nums2):   ## written by my own, solve question3, encountered limited memory
+    ret = []
+    nums1.sort()
+    nums2.sort()
+    i = 0
+    j = 0
+    while i < len(nums1) and j < len(nums2):   ## 和q349不一样的地方在于，重复的也需要添加
+        while j < len(nums2) and nums2[j] < nums1[i]:  ## 记住只有目前小于nums1的时候，才需要一直挪
+            j = j+1
+        if j < len(nums2) and nums2[j] == nums1[i]:
+            ret.append(nums1[i])
+            j = j+1    ## 如果nums2中有一个加进来了，才需要把这个往后挪1
+        i = i+1  ## 相当于nums1中的每个数都需要找一遍看看nums2剩下的一部分有没有和这个数相同的
     return ret
 
 a= intersect(nums1 = [1,2,2,1], nums2 = [2,2])
